be4759ab0994927d2eb3475e04aa420e7cca4269
306-san <oh,taku528+git@gmail.com>
2017-04-22 16:46:33 +0900

    6 yatta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
+import Dog from './dog';
+
 const str = 'ES6';
 console.log(`Hello ${str}`);
-import Dog from './dog';
 const toby = new Dog('Toby');
 console.log(toby.bark());
